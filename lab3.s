9734924d45b69042808a49ddc86ab30cd3c2ba5b
Eli <elimelec.burghelea@gmail.com>
2016-03-07 09:37:07 +0200

    Create procedure

--- lab3.s
+++ lab3.s
@@ -1,50 +1,44 @@
 .data
-numere_prime:
-	.word 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
 
 .text
 	#read number
 	li $v0, 5
 	syscall
 	
-	#result stored in $v0
+	jal is_num_prime
+	
+	move $a0, $v0
+	li $v0, 1
+	syscall
+	
+
+end_prog:
+	done
+	
+	
+# $v0 <- num
+# $v0 -> 1 if prime	else 0
+is_num_prime:
 	move $t0, $v0
-	
-	# == 2
-	li $t2, 2
-	beq $t0, $t2, prim
-	
-	# == 3
-	li $t2, 3
-	beq $t0, $t2, prim
 	
 	#check for prime
 	li $t1, 2
-loop_prime:
-	beq $t0, $t1, prim
+is_num_prime__loop:
+	beq $t0, $t1, is_num_prime__prime
 	
 	div $t0, $t1
 	mfhi $t2
-	beqz $t2, ne_prim
+	beqz $t2, is_num_prime__not_prime
 	
 	add $t1, $t1, 1
-	b loop_prime
+	b is_num_prime__loop
 	
-prim:
-	#print number
-	li $a0, 1
+is_num_prime__prime:
 	li $v0, 1
-	syscall
-	b end_prog
+	jr $ra
 
-ne_prim:
-	#print number
-	li $a0, 0
-	li $v0, 1
-	syscall
-	b end_prog
-
-end_prog:	
-	done
+is_num_prime__not_prime:
+	li $v0, 0
+	jr $ra
 	
 	
